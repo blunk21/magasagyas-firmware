3e9b63a6bb0b04f92a348c602360149ed5cc46f2
blunk21 <g.bereczki21@gmail.com>
2023-12-30 19:53:25 +0100

    .

--- main.cpp
+++ main.cpp
@@ -5,21 +5,6 @@
 #include <cstdint>
 #include <cstdio>
 
-void save_sensor_data(void){
-    DataManager::getDataManager().storeSensorData();
-}
-
-
 // main() runs in its own thread in the OS
-int main() {
-    Thread dataManagerThread(osPriorityHigh,8192,nullptr,"datamanager_thread");
-    DataManager::getDataManager();
-    SensorData &sensors = SensorData::getSensorData();
-    dataManagerThread.start(save_sensor_data);
-    
-    while (true) {
-        sensors.readSensorData();
-        ThisThread::sleep_for(5s);
-    }
-  
-}
+int main() {}
+
