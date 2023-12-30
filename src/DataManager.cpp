486f946996e041d945b49b3985680c38b839b4c1
blunk21 <g.bereczki21@gmail.com>
2023-12-30 19:51:50 +0100

    added print of sensor data

--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -4,9 +4,13 @@
 
 DataManager::DataManager()
     : _bd(SPI_FLASH_MOSI, SPI_FLASH_MISO, SPI_FLASH_CLK, SPI_FLASH_CS),
-      _fs(FILESYSTEM_MOUNTING_POINT) {}
-
-DataManager::~DataManager() {}
+      _fs(FILESYSTEM_MOUNTING_POINT) {
+          printf("initializing block device\n");
+          int success = _bd.init();
+          if(!success) printf("Blockdevice initialised successfully\n");
+          else printf("Failed to init block device\n");
+          _fs.format(&_bd);
+      }
 
 DataManager &DataManager::getDataManager() {
   static DataManager instance;
@@ -16,7 +20,7 @@ DataManager &DataManager::getDataManager() {
 void DataManager::storeSensorData() {
   sensor_measurement_t *measurement = sensor_data_mailbox.try_get();
   if (measurement == NULL) {
-    printf("Not sensor data available\n");
+    printf("No sensor data available\n");
     return;
   }
   bool success = mountFileSystem();
@@ -24,6 +28,9 @@ void DataManager::storeSensorData() {
     printf("Could not mount file system. Unable to store sensor data.\n\r");
     // file_system_mutex.unlock();
     return;
+  }
+  else {
+  printf("Filesystem mounted successfully\n");
   }
   // file_system_mutex.lock();
   FILE *file = fopen("/fs/sensor_data.csv", "a");
@@ -33,7 +40,7 @@ void DataManager::storeSensorData() {
     return;
   } else {
     printf("Writing sensor data to file.\n\r");
-    fprintf(file, "%ld,%ld,%ld,%d,%s\n", measurement->temperature,
+    fprintf(file, "%ld,%ld,%ld,%d,%d\n", measurement->temperature,
             measurement->air_humidity, measurement->air_pressure,
             measurement->soil_humidity, measurement->water_level_critical);
     // fprintf(file, "%ld,%ld,%ld,%d,%s\n", measurement->temperature,
@@ -58,4 +65,32 @@ bool DataManager::mountFileSystem() {
 
   _fs.reformat(&_bd);
   return false;
+}
+
+void DataManager::printSensorMeasurements()
+{
+     FILE *file = fopen("/fs/sensor_data.csv", "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char *buffer = (char *)malloc(fileSize + 1);
+    if (buffer == NULL) {
+        perror("Error allocating memory");
+        fclose(file);
+        return;
+    }
+
+    fread(buffer, 1, fileSize, file);
+    buffer[fileSize] = '\0';
+
+    printf("%s", buffer);
+
+    fclose(file);
+    free(buffer);
 }
