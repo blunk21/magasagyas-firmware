a836f7c6fc990223ad3d5e7141e6938087a92d07
blunk21 <g.bereczki21@gmail.com>
2024-01-06 14:55:08 +0100

    basic setup

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "IrrigationManager.h"
 #include "SensorData.h"
 #include "SystemConfig.h"
+
 #include "mbed.h"
 #include <chrono>
 #include <cstddef>
@@ -11,17 +12,24 @@
 #include <ctime>
 
 int main() {
-  set_time(1704530047);
+  set_time(1704520800);
   // instantiate singletons
   SystemConfig::getSystemConfig();
   EventQueue *global_queue = GlobalEventQueue::getGlobalEventQueue();
-
+  IrrigationManager::getIrrigationManager();
+  global_queue->call_every(
+      60s, callback(&IrrigationManager::getIrrigationManager(),
+                    &IrrigationManager::scheduleNextIrrigationTime));
   global_queue->call_every(
       std::chrono::seconds(SENSOR_READ_INTERVAL_SECONDS),
       callback(&SensorData::getSensorData(), &SensorData::readSensorData));
-
+  uint32_t tick_counter = 0;
   while (true) {
 
     ThisThread::sleep_for(1s);
+    tick_counter++;
+    if (tick_counter % 5 == 0) {
+      printf("Ticks %d\n", tick_counter);
+    }
   }
 }
