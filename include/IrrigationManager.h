ad7dd4b4afd71d5487337f94be67cee8be21368a
blunk21 <g.bereczki21@gmail.com>
2024-01-06 10:21:15 +0100

    starting and stopping irrigation

---
+++ include/IrrigationManager.h
@@ -0,0 +1,35 @@
+#ifndef IRRIGATION_MANAGER_H
+#define IRRIGATION_MANAGER_H
+#include <cstdint>
+#define PUMP_RELAY_PIN PB_4
+#include "mbed.h"
+#include "time.h"
+
+enum alarm_duration {
+  ALARM_DURATION_A,
+  ALARM_DURATION_B
+
+};
+
+class IrrigationManager {
+private:
+  bool _irrigation_in_progress;
+  Timeout irrigation_stop_timer;
+  DigitalOut _pump_relay;
+  bool _initialized;
+  EventQueue *_event;
+  uint8_t _next_alarm;
+  IrrigationManager();
+
+  time_t calculateNextIrrigationTime();
+
+public:
+  static IrrigationManager &getIrrigationManager();
+  void startIrrigation(uint32_t duration);
+  void stopIrrigation();
+  void attachEventQueue(EventQueue *event_p);
+  void init();
+  void releaseIrrigation();
+};
+
+#endif
